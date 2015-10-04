9887fc0e2f9771c075377eb4bd0ccb3e182a1e84
rodamber <rodrigoambernardo@gmail.com>
2015-10-04 20:27:49 +0100

    Saner Vector3 implementation.
    Check operator== for the FIXME.

--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -1,66 +1,44 @@
 #include "Vector3.hpp"
 
-Vector3::Vector3()
-{
+Vector3::Vector3() : _x(0), _y(0), _z(0) {}
+
+Vector3::Vector3(GLdouble x, GLdouble y, GLdouble z) {
+    _x = x;
+    _y = y;
+    _z = z;
 }
 
-Vector3::Vector3(GLdouble x, GLdouble y, GLdouble z)
-{
-	_x = x;
-	_y = y;
-	_z = z;
+Vector3::~Vector3() {}
+
+GLdouble Vector3::getX() const { return _x; }
+GLdouble Vector3::getY() const { return _y; }
+GLdouble Vector3::getZ() const { return _z; }
+
+void Vector3::set(GLdouble x, GLdouble y, GLdouble z) { _x = x; _y = y; _z = z; }
+
+Vector3 Vector3::operator=(const Vector3 & vec) {
+    _x = vec.getX();
+    _y = vec.getY();
+    _z = vec.getZ();
+
+    return *this;
+
+    // Fixme: Shouldn't this be the right semantics?
+    // return Vector3(vec.getX(), vec.getY(), vec.getZ());
 }
 
-Vector3::~Vector3()
-{
+Vector3 Vector3::operator*(GLdouble num) const {
+    return Vector3(getX() * num, getY() * num, getZ() * num);
 }
 
-GLdouble Vector3::getX() const
-{
-	return _x;
+Vector3 Vector3::operator+(const Vector3& vec) const {
+    return Vector3(getX() + vec.getX(), getY() + vec.getY(), getZ() + vec.getZ());
 }
 
-GLdouble Vector3::getY() const
-{
-	return _y;
+Vector3 Vector3::operator-(const Vector3& vec) const {
+    return Vector3(getX() - vec.getX(), getY() - vec.getY(), getZ() - vec.getZ());
 }
 
-GLdouble Vector3::getZ() const
-{
-	return _z;
-}
-
-void Vector3::set(GLdouble x, GLdouble y, GLdouble z)
-{
-	_x = x;
-	_y = y;
-	_z = z;
-}
-
-Vector3 Vector3::operator=(const Vector3 & vec)
-{
-	_x = vec.getX();
-	_y = vec.getY();
-	_z = vec.getZ();
-	return *this;
-}
-
-Vector3 Vector3::operator*(GLdouble num)
-{
-	return Vector3(this->getX() * num, this->getY() * num, this->getZ() * num);
-}
-
-Vector3 Vector3::operator+(const Vector3& vec)
-{
-	return Vector3(this->getX() + vec.getX(), this->getY() + vec.getY(), this->getZ() + vec.getZ());
-}
-
-Vector3 Vector3::operator-(const Vector3& vec)
-{
-	return Vector3(this->getX() - vec.getX(), this->getY() - vec.getY(), this->getZ() - vec.getZ());
-}
-
-void Vector3::glTranslate()
-{
-	glTranslatef( _x, _y, _z );
+void Vector3::glTranslate() const {
+    glTranslatef( _x, _y, _z );
 }
--- src/Vector3.hpp
+++ src/Vector3.hpp
@@ -6,20 +6,22 @@ protected:
     GLdouble _x;
     GLdouble _y;
     GLdouble _z;
-
 public:
     Vector3();
     Vector3(GLdouble x, GLdouble y, GLdouble z);
     ~Vector3();
+
     GLdouble getX() const;
     GLdouble getY() const;
     GLdouble getZ() const;
-    void set(GLdouble x, GLdouble y, GLdouble z);
-    Vector3 operator=(const Vector3& vec);
-    Vector3 operator*(GLdouble num);
-    Vector3 operator+(const Vector3&);
-    Vector3 operator-(const Vector3&);
-    /** glTranslate using this vector */
-    void glTranslate();
 
+    void set(GLdouble x, GLdouble y, GLdouble z);
+
+    Vector3 operator=(const Vector3& vec);
+    Vector3 operator*(GLdouble num)   const;
+    Vector3 operator+(const Vector3&) const;
+    Vector3 operator-(const Vector3&) const;
+
+    /** glTranslate using this vector */
+    void glTranslate() const;
 };
