1ff1d7bcdf1a1cb8a0fc9e3a317296154d9c8397
rodamber <rodrigoambernardo@gmail.com>
2015-10-04 18:42:06 +0100

    Getters in Entity are now const

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,26 +1,17 @@
 #include "Entity.hpp"
 
-Entity::Entity() : _position(0, 0, 0)
-{
+Entity::Entity() : _position(0.0d, 0.0d, 0.0d) {}
 
-}
+Entity::~Entity() {}
 
-Entity::~Entity()
-{
-
-}
-
-Vector3& Entity::getPosition()
-{
+Vector3& Entity::getPosition() const {
     return _position;
 }
 
-void Entity::setPosition(GLdouble x, GLdouble y, GLdouble z)
-{
+void Entity::setPosition(GLdouble x, GLdouble y, GLdouble z) {
     _position.set( x, y, z );
 }
 
-void Entity::setPosition(const Vector3 & p)
-{
+void Entity::setPosition(const Vector3 & p) {
     _position = p;
 }
--- src/Entity.hpp
+++ src/Entity.hpp
@@ -3,12 +3,15 @@
 #include "Vector3.hpp"
 #include "global.hpp"
 
+
 class Entity {
 	Vector3 _position;
 public:
 	Entity();
 	~Entity();
-	Vector3& getPosition();
+
+	Vector3& getPosition() const;
+
 	void setPosition(GLdouble n, GLdouble y, GLdouble z);
 	void setPosition(const Vector3& p);
 
