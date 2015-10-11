7a7a6866c83dc1e9e8560650c7fe309d8e3bb471
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-10-11 19:03:49 +0100

    :fish: Removed comment.

--- src/Roadside.hpp
+++ src/Roadside.hpp
@@ -10,12 +10,6 @@ public:
     Roadside( GLdouble size );
     virtual ~Roadside();
     virtual void draw();
-    /*
-    virtual void setDrawAsWireframe(bool val) override {
-        std::cout << "HELLO!" << std::endl;
-        GameObjectCollection::setDrawAsWireframe(val);
-    }
-    */
         
 private:
     GLdouble _size;
