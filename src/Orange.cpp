5f339fda31d4ebadae10d7b95fb5c16ba8d64379
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2015-10-19 19:46:12 +0100

    implemented Orange stalk

--- src/Orange.cpp
+++ src/Orange.cpp
@@ -4,16 +4,30 @@ Orange::Orange(GLdouble radius) : Orange(radius, 0, 0, 0) {}
 
 Orange::Orange(GLdouble radius, GLdouble x, GLdouble y, GLdouble z) : _radius(radius) {
     setPosition(x, y, z);
+    _stalk_width  = radius / 20.0f;
+    _stalk_height = radius / 3.0f;
 }
+
 
 void Orange::draw() {
     const GLint num_slices = 20;
     const GLint num_slacks = 20;
 
     glPushMatrix();
-    GameObject::draw();
+    GameObject::draw(); // translate to position
 
     glColor3f( components3( OrangeColors::skin ));
     drawSphere(_radius, num_slices, num_slacks);
+
+
+    glPushMatrix();
+    { // draw orange stalk
+        glTranslatef( 0.0f, 0.0f, _stalk_height / 2.0f + _radius );
+        glColor3f( components3( OrangeColors::stalk ));
+        glScalef( _stalk_width, _stalk_width, _stalk_height );
+        drawCube( 1.0f );
+    }
+    glPopMatrix();
+
     glPopMatrix();
 }
--- src/Orange.hpp
+++ src/Orange.hpp
@@ -6,11 +6,12 @@
 
 namespace OrangeColors {
     static constexpr GLfloat skin[3]  = { 1.0f, 0.627451f, 0.0f }; // Orange
+    static constexpr GLfloat stalk[3] = { 0.000f, 0.392f, 0.000f }; // DarkGreen
 }
 
 class Orange : public Obstacle {
 private:
-    GLdouble _radius;
+    GLdouble _radius, _stalk_width, _stalk_height;
 public:
     Orange(GLdouble radius);
     Orange(GLdouble radius, GLdouble x, GLdouble y, GLdouble z);
