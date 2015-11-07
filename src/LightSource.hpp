785c68843027c8fb63920209d74e045ec3eb0a29
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-11-07 11:12:46 +0000

    LightSource class added. Closes #57.
    NOTE: draw() not yet implemented.

---
+++ src/LightSource.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "global.hpp"
+#include "Vector4.hpp"
+#include "Vector3.hpp"
+
+class LightSource {
+private:
+    GLdouble _ambient[4];
+    GLdouble _diffuse[4];
+    GLdouble _specular[4];
+    GLdouble _cut_off;
+    GLdouble _exponent;
+    GLenum _num;
+    GLboolean _state;
+    Vector3 _direction;
+    Vector3 _position;
+
+public:
+    LightSource(GLdouble number);
+    ~LightSource();
+    GLboolean getState() const;
+    void setState(GLboolean state);
+    GLenum getNum() const;
+    void setPosition(const Vector3& pos);
+    void setDirection(const Vector3& dir);
+    void setCutoff(GLdouble cut_off);
+    void setExponent(GLdouble exp);
+    void setAmbient(const Vector4& amb);
+    void setDiffuse(const Vector4& diff);
+    void setSpecular(const Vector4& spec);
+    void draw();
+};
