cbb9740a9c336f1537a0840c0f56c6c1ad88b96d
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-11-07 10:25:04 +0000

    Vector4 implemented. Closes #58

---
+++ src/Vector4.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "global.hpp"
+#include <iostream>
+
+class Vector4
+{
+protected:
+    GLdouble _x;
+    GLdouble _y;
+    GLdouble _z;
+    GLdouble _t;
+public:
+    Vector4();
+    Vector4(GLdouble x, GLdouble y, GLdouble z, GLdouble t);
+    ~Vector4();
+
+    GLdouble getX() const;
+    GLdouble getY() const;
+    GLdouble getZ() const;
+    GLdouble getT() const;
+
+    void set(GLdouble x, GLdouble y, GLdouble z, GLdouble t);
+    GLdouble norm() const;
+    Vector4 normalized() const;
+
+    friend std::ostream &operator<<(std::ostream&, const Vector4&);
+    Vector4 operator=(const Vector4& vec);
+    Vector4 operator*(GLdouble num)   const;
+    Vector4 operator+(const Vector4&) const;
+    Vector4 operator-(const Vector4&) const;
+    bool operator==(const Vector4 &) const;
+    bool operator!=(const Vector4 &) const;
+
+};
+
+const Vector4 zeroVector(0, 0, 0, 0);
