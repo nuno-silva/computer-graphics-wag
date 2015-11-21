dce30f313a9708929a761e957cd5c3a76b86c6c9
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-11-21 17:03:06 +0000

    Moved glut include decision from global.hpp to a separate file.
    In some places all we really need is to include the glut library, it
    doesn't make much sense to include everything contained in global.hpp in
    those files.

--- src/global.hpp
+++ src/global.hpp
@@ -1,18 +1,12 @@
 #pragma once
 
+#include "glut.hpp"
+
 #define P_CAM_FOV_Y  45
 
 #define PI 3.14159265358979323846
 
 #define pow2(x) pow(x, 2.0f)
-
-// we need glut in almost all files...
-#ifdef __MACH__
-# include <GLUT/glut.h>
-#else
-# include <stdlib.h>
-# include <GL/glut.h>
-#endif
 
 /* usefull unit conversions, assuming one unit is one meter */
 #define mm(x) ( (x) / 1000.0f ) // millimeter to meter
